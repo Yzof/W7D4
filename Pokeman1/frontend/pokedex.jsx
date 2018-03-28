812340024776ff62bb58a7638c1ac599e8f69918
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-03-28 14:45:21 -0700

    Create Reducers and Thunk

--- Pokeman1/frontend/pokedex.jsx
+++ Pokeman1/frontend/pokedex.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions.js';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("root");
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.receiveAllPokemon = receiveAllPokemon;
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
+
+
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions.js';
+window.fetchAllPokemon = fetchAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
